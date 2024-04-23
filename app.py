c4f91fc04d5e874bbe010fd9679b77ebe4d008b3
brycedemopoulos <brycedemopoulos@gmail.com>
2024-04-23 16:59:34 -0400

    app.py

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ app = MultiApp()
 st.markdown("""
 # Surgery Timings Database
 
-This multi-page app was created using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework. Create the graph of your dreams using the new interactive Surgey Timings Database. 
-
-""")
+This multi-page app was created using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework. """)
 
 # Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Real Data", real.app)
-app.add_app("Fake Data", fake.app)
+app.add_app("Deformity Cases of 3 Surgeons", real.app)
+app.add_app("Example Data", fake.app)
 # The main app
 app.run()
